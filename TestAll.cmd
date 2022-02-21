c1e9d39f3913f40347f5a6b0fe0f5cea2b5b64e9
Igor Popov <popov.nirvana@gmail.com>
2022-02-21 11:57:34 +0300

    Fix workflow

--- TestAll.cmd
+++ TestAll.cmd
@@ -2,12 +2,12 @@ SET PLATFORM=Win32
 SET CONFIG=Debug
 
 IF NOT "%1"=="" SET PLATFORM=%1
-IF NOT "%2"=="" SET CONFIG=%2
+IF NOT "%2"=="" SET CONFIG=%2 %3
 
-%PLATFORM%\%CONFIG%\TestLibrary.exe --gtest_output=xml:TestLibrary.%PLATFORM%.%CONFIG%.xml
-%PLATFORM%\%CONFIG%\TestORB.exe --gtest_output=xml:TestORB.%PLATFORM%.%CONFIG%.xml
-%PLATFORM%\%CONFIG%\TestSTL.exe --gtest_output=xml:TestSTL.%PLATFORM%.%CONFIG%.xml
-%PLATFORM%\%CONFIG%\TestWindowsAPI.exe --gtest_output=xml:TestWindowsAPI.%PLATFORM%.%CONFIG%.xml
-%PLATFORM%\%CONFIG%\TestWindows.exe --gtest_output=xml:TestWindows.%PLATFORM%.%CONFIG%.xml
-%PLATFORM%\%CONFIG%\TestCore.exe --gtest_output=xml:TestCore.%PLATFORM%.%CONFIG%.xml
-%PLATFORM%\%CONFIG%\Nirvana.exe -s TestProcess.nex --gtest_catch_exceptions=0 --gtest_output=xml:TestProcess.%PLATFORM%.%CONFIG%.xml
+"%PLATFORM%\%CONFIG%\TestLibrary.exe" "--gtest_output=xml:TestLibrary.%PLATFORM%.%CONFIG%.xml"
+"%PLATFORM%\%CONFIG%\TestORB.exe" "--gtest_output=xml:TestORB.%PLATFORM%.%CONFIG%.xml"
+"%PLATFORM%\%CONFIG%\TestSTL.exe" "--gtest_output=xml:TestSTL.%PLATFORM%.%CONFIG%.xml"
+"%PLATFORM%\%CONFIG%\TestWindowsAPI.exe" "--gtest_output=xml:TestWindowsAPI.%PLATFORM%.%CONFIG%.xml"
+"%PLATFORM%\%CONFIG%\TestWindows.exe" "--gtest_output=xml:TestWindows.%PLATFORM%.%CONFIG%.xml"
+"%PLATFORM%\%CONFIG%\TestCore.exe" "--gtest_output=xml:TestCore.%PLATFORM%.%CONFIG%.xml"
+"%PLATFORM%\%CONFIG%\Nirvana.exe" "-s TestProcess.nex --gtest_catch_exceptions=0 --gtest_output=xml:TestProcess.%PLATFORM%.%CONFIG%.xml"
