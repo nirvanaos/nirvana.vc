952e54c64f38522a820761605d3cb93f4a291208
Igor Popov <popov.nirvana@gmail.com>
2022-12-29 14:16:50 +0300

    Fix test script

--- TestAll.cmd
+++ TestAll.cmd
@@ -14,7 +14,7 @@ TestWindows.exe "--gtest_output=xml:..\..\TestWindows.%PLATFORM%.%CONFIG%.xml"
 TestCore.exe "--gtest_output=xml:..\..\TestCore.%PLATFORM%.%CONFIG%.xml"
 
 start "" "..\..\x64\%CONFIG%\Nirvana.exe" -s
-timeout /t 1 /nobreak
+waitfor /t 1 SignalName
 Nirvana.exe TestProcess.nex --gtest_catch_exceptions=0 "--gtest_output=xml:..\..\TestProcess.%PLATFORM%.%CONFIG%.xml"
 Nirvana.exe TestSystem.nex --gtest_catch_exceptions=0 "--gtest_output=xml:..\..\TestSystem.%PLATFORM%.%CONFIG%.xml"
 Nirvana.exe TestFixed.nex --gtest_catch_exceptions=0 "--gtest_output=xml:..\..\TestFixed.%PLATFORM%.%CONFIG%.xml"
