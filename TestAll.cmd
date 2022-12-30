5a9618bb1f8d0be000a89700e44b6213e5a21efd
Igor Popov <popov.nirvana@gmail.com>
2022-12-30 09:35:48 +0300

    Fix TestAll

--- TestAll.cmd
+++ TestAll.cmd
@@ -5,9 +5,11 @@ IF NOT "%1"=="" SET PLATFORM=%1
 IF NOT "%2"=="" SET CONFIG=%2
 IF NOT "%3"=="" SET CONFIG=%2 %3
 
+echo on
+
 cd "%PLATFORM%\%CONFIG%"
-rem TestLibrary.exe "--gtest_output=xml:..\..\TestLibrary.%PLATFORM%.%CONFIG%.xml"
-rem TestORB.exe "--gtest_output=xml:..\..\TestORB.%PLATFORM%.%CONFIG%.xml"
+TestLibrary.exe "--gtest_output=xml:..\..\TestLibrary.%PLATFORM%.%CONFIG%.xml"
+TestORB.exe "--gtest_output=xml:..\..\TestORB.%PLATFORM%.%CONFIG%.xml"
 rem TestSTL.exe "--gtest_output=xml:..\..\TestSTL.%PLATFORM%.%CONFIG%.xml"
 rem TestWindowsAPI.exe "--gtest_output=xml:..\..\TestWindowsAPI.%PLATFORM%.%CONFIG%.xml"
 rem TestWindows.exe "--gtest_output=xml:..\..\TestWindows.%PLATFORM%.%CONFIG%.xml"
@@ -15,7 +17,7 @@ rem TestCore.exe "--gtest_output=xml:..\..\TestCore.%PLATFORM%.%CONFIG%.xml"
 
 start "" /b "..\..\x64\%CONFIG%\Nirvana.exe" -s
 
-ping -n 1 127.0.0.1
+ping -n 2 127.0.0.1
 
 Nirvana.exe TestProcess.nex --gtest_catch_exceptions=0 "--gtest_output=xml:..\..\TestProcess.%PLATFORM%.%CONFIG%.xml"
 Nirvana.exe TestSystem.nex --gtest_catch_exceptions=0 "--gtest_output=xml:..\..\TestSystem.%PLATFORM%.%CONFIG%.xml"
