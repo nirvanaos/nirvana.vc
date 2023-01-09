0b63acaa852375d0773461d48214edcd9f5ed6e3
Igor Popov <popov.nirvana@gmail.com>
2023-01-09 16:43:20 +0300

    Fix test script

--- BuildAndTest.ps1
+++ BuildAndTest.ps1
@@ -15,19 +15,19 @@ cd "$platform\\$config"
 
 $test_result="..\..\test-results\$platform.$config"
 
-#.\TestLibrary.exe "--gtest_output=xml:$test_result.TestLibrary.xml"
-#.\TestORB.exe "--gtest_output=xml:$test_result.TestORB.xml"
-#.\TestSTL.exe "--gtest_output=xml:$test_result.TestSTL.xml"
-#.\TestWindowsAPI.exe "--gtest_output=xml:$test_result.TestWindowsAPI.xml"
-#.\TestWindows.exe "--gtest_output=xml:$test_result.TestWindows.xml"
-#.\TestCore.exe "--gtest_output=xml:$test_result.TestCore.xml"
+.\TestLibrary.exe "--gtest_output=xml:$test_result.TestLibrary.xml"
+.\TestORB.exe "--gtest_output=xml:$test_result.TestORB.xml"
+.\TestSTL.exe "--gtest_output=xml:$test_result.TestSTL.xml"
+.\TestWindowsAPI.exe "--gtest_output=xml:$test_result.TestWindowsAPI.xml"
+.\TestWindows.exe "--gtest_output=xml:$test_result.TestWindows.xml"
+.\TestCore.exe "--gtest_output=xml:$test_result.TestCore.xml"
 
 Start-Process -NoNewWindow -FilePath "..\..\x64\$config\Nirvana.exe" -ArgumentList "-s"
 Start-Sleep -Seconds 2
 
 Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestProcess.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestProcess.xml`""
-#Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "TestSystem.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestSystem.xml`""
-#Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "TestFixed.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestFixed.xml`""
+Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestSystem.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestSystem.xml`""
+Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestFixed.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestFixed.xml`""
 
 Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "-d"
 
