b304950cfe19a903e39a6da7f6c3bd6924a8944a
Igor Popov <popov.nirvana@gmail.com>
2023-01-09 15:51:03 +0300

    Debug script

--- BuildAndTest.ps1
+++ BuildAndTest.ps1
@@ -13,20 +13,22 @@ msbuild -p:Platform="$platform" -p:Configuration="$config"
 
 cd "$platform\\$config"
 
-#.\TestLibrary.exe "--gtest_output=xml:..\..\TestLibrary.$platform.$config.xml"
-#.\TestORB.exe "--gtest_output=xml:..\..\TestORB.$platform.$config.xml"
-#.\TestSTL.exe "--gtest_output=xml:..\..\TestSTL.$platform.$config.xml"
-#.\TestWindowsAPI.exe "--gtest_output=xml:..\..\TestWindowsAPI.$platform.$config.xml"
-#.\TestWindows.exe "--gtest_output=xml:..\..\TestWindows.$platform.$config.xml"
-#.\TestCore.exe "--gtest_output=xml:..\..\TestCore.$platform.$config.xml"
+$test_result="..\..\testres\$platform.$config"
+
+#.\TestLibrary.exe "--gtest_output=xml:$test_result.TestLibrary.xml"
+#.\TestORB.exe "--gtest_output=xml:$test_result.TestORB.xml"
+#.\TestSTL.exe "--gtest_output=xml:$test_result.TestSTL.xml"
+#.\TestWindowsAPI.exe "--gtest_output=xml:$test_result.TestWindowsAPI.xml"
+#.\TestWindows.exe "--gtest_output=xml:$test_result.TestWindows.xml"
+#.\TestCore.exe "--gtest_output=xml:$test_result.TestCore.xml"
 
 Set-PSDebug -Trace 2
 Start-Process -FilePath "..\..\x64\$config\Nirvana.exe" -ArgumentList "-s"
 Start-Sleep -Seconds 2
 
-Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "TestProcess.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:..\..\TestProcess.$platform.$config.xml`""
-Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "TestSystem.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:..\..\TestSystem.$platform.$config.xml`""
-Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "TestFixed.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:..\..\TestFixed.$platform.$config.xml`""
+Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestProcess.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestProcess.xml`""
+#Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "TestSystem.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestSystem.xml`""
+#Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "TestFixed.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestFixed.xml`""
 
 Start-Process -Wait -FilePath ".\Nirvana.exe" -ArgumentList "-d"
 
