633d45a6abf08e3eacac4c62ad38e41862df4a70
Igor Popov <popov.nirvana@gmail.com>
2023-01-17 12:30:59 +0300

    Debug test script

--- BuildAndTest.ps1
+++ BuildAndTest.ps1
@@ -19,17 +19,18 @@ cd "$platform\\$config"
 
 $test_result="..\..\test-results\$platform.$config"
 
-.\TestLibrary.exe "--gtest_output=xml:$test_result.TestLibrary.xml"
-.\TestORB.exe "--gtest_output=xml:$test_result.TestORB.xml"
-.\TestSTL.exe "--gtest_output=xml:$test_result.TestSTL.xml"
-.\TestWindowsAPI.exe "--gtest_output=xml:$test_result.TestWindowsAPI.xml"
-.\TestWindows.exe "--gtest_output=xml:$test_result.TestWindows.xml"
+#.\TestLibrary.exe "--gtest_output=xml:$test_result.TestLibrary.xml"
+#.\TestORB.exe "--gtest_output=xml:$test_result.TestORB.xml"
+#.\TestSTL.exe "--gtest_output=xml:$test_result.TestSTL.xml"
+#.\TestWindowsAPI.exe "--gtest_output=xml:$test_result.TestWindowsAPI.xml"
+#.\TestWindows.exe "--gtest_output=xml:$test_result.TestWindows.xml"
 #.\TestCore.exe "--gtest_output=xml:$test_result.TestCore.xml"
 
 $appdata = [Environment]::GetFolderPath('CommonApplicationData')
 
 Write-Host "Start Nirvana"
-Start-Process -NoNewWindow -FilePath "..\..\x64\$config\Nirvana.exe" -ArgumentList "-s"
+$sysdomain = (Start-Process -NoNewWindow -PassThru -FilePath "..\..\x64\$config\Nirvana.exe" -ArgumentList "-s")
+Write-Host $sysdomain.id
 
 $started = $false
 for ($i = 0; $i -lt 4; $i++) {
@@ -50,7 +51,6 @@ if ($started) {
 }
 
 Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestProcess.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestProcess.xml`""
-Write-Output $LASTEXITCODE
 #Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestSystem.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestSystem.xml`""
 #Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestFixed.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestFixed.xml`""
 
@@ -58,3 +58,8 @@ Write-Host "Stop Nirvana"
 Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "-d"
 
 cd ..\..
+
+Try { Wait-Process -InputObject $sysdomain } Catch {}
+Write-Host "System domain exit code:" $sysdomain.GetType().GetField('exitCode', 'NonPublic, Instance').GetValue($sysdomain)
+#$sysdomain.ExitCode
+
