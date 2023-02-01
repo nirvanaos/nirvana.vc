5947f0d47f6c24669ab2c93502cd46e09e829ac8
Igor Popov <popov.nirvana@gmail.com>
2023-02-01 06:38:36 +0300

    Debug

--- BuildAndTest.ps1
+++ BuildAndTest.ps1
@@ -19,11 +19,11 @@ cd "$platform\\$config"
 
 $test_result="..\..\test-results\$platform.$config"
 
-#.\TestLibrary.exe "--gtest_output=xml:$test_result.TestLibrary.xml"
-#.\TestORB.exe "--gtest_output=xml:$test_result.TestORB.xml"
-#.\TestSTL.exe "--gtest_output=xml:$test_result.TestSTL.xml"
-#.\TestWindowsAPI.exe "--gtest_output=xml:$test_result.TestWindowsAPI.xml"
-#.\TestWindows.exe "--gtest_output=xml:$test_result.TestWindows.xml"
+.\TestLibrary.exe "--gtest_output=xml:$test_result.TestLibrary.xml"
+.\TestORB.exe "--gtest_output=xml:$test_result.TestORB.xml"
+.\TestSTL.exe "--gtest_output=xml:$test_result.TestSTL.xml"
+.\TestWindowsAPI.exe "--gtest_output=xml:$test_result.TestWindowsAPI.xml"
+.\TestWindows.exe "--gtest_output=xml:$test_result.TestWindows.xml"
 #.\TestCore.exe "--gtest_output=xml:$test_result.TestCore.xml"
 
 $appdata = [Environment]::GetFolderPath('CommonApplicationData') + "\Nirvana\Nirvana"
@@ -51,8 +51,8 @@ if ($started) {
 	Exit
 }
 
-#Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestProcess.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestProcess.xml`""
-#Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestSystem.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestSystem.xml`""
+Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestProcess.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestProcess.xml`""
+Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestSystem.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestSystem.xml`""
 Start-Process -Wait -NoNewWindow -FilePath ".\Nirvana.exe" -ArgumentList "TestFixed.nex --gtest_catch_exceptions=0 `"--gtest_output=xml:$test_result.TestFixed.xml`""
 
 Write-Host "Stop Nirvana"
