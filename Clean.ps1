7dbba2fbfa6742cae4da66cf53f372488a61cdd9
Igor Popov <popov.nirvana@gmail.com>
2023-06-23 12:27:35 +0300

    Fix

---
+++ Clean.ps1
@@ -0,0 +1,12 @@
+if ($args.count -ge 1) {
+	$platform = $args[0]
+} else {
+	$platform = "x64"
+}
+if ($args.count -ge 1) {
+	$config = $args[1]
+} else {
+	$config = "Debug 2022"
+}
+
+msbuild -target:Clean -p:Platform="$platform" -p:Configuration="$config"
